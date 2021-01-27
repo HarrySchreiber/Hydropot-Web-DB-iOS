ce3db81f1de36912a60a675bead6e5b65cfe116c
draydr <draydr17@gcc.edu>
2021-01-27 15:01:40 -0500

    Created an iOS app

---
+++ iOS App/HydroPot/HydroPot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HydroPot
+//
+//  Created by David Dray on 1/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS App/HydroPot/HydroPot/HydroPotApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydroPotApp.swift
+//  HydroPot
+//
+//  Created by David Dray on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct HydroPotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
