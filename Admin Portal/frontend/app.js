d1427d5b2f504f75834e8eda217512931d7e9cdf
HarrySchreiber <44842528+HarrySchreiber@users.noreply.github.com>
2021-01-31 21:23:15 -0500

    Refactored a bunch and now everything works with divs I think

--- Admin Portal/frontend/app.js
+++ Admin Portal/frontend/app.js
@@ -36,22 +36,66 @@ fetch(API_URL)
 function buildTable(data){
     var keyArray = ["name","idealTempHigh","idealTempLow","idealMoistureHigh","idealMoistureLow","idealLightHigh","idealLightLow","description"];
     for(var i = 0; i < data.length; i++){
+        //Declare the json object
         var obj = data[i];
-        var row = document.createElement("div");
-        row.setAttribute("class","row no-gutters");
+        //Declare Columns and rows
+        var fullRow = document.createElement("div");
+        fullRow.setAttribute("class","row no-gutters");
+        var pictureCol = document.createElement("div");
+        pictureCol.setAttribute("class", "col-md-1 no-gutters");
+        var contentCol = document.createElement("div");
+        contentCol.setAttribute("class", "col-md-10 no-gutters");
+        var buttonsCol = document.createElement("div");
+        buttonsCol.setAttribute("class", "col-md-1 no-gutters");
+
+        //TODO: Image Code
+        
+
+        //Content Code
+        var topRow = document.createElement("div");
+        topRow.setAttribute("class","row no-gutters");
+        var bottomRow = document.createElement("div");
+        bottomRow.setAttribute("class","row no-gutters");
         for(var key in keyArray){
-            var col = document.createElement("div");
-            col.setAttribute("class","col-sm-1");
             var input = document.createElement("input");
-            if(keyArray[key] == "name" || keyArray[key] == "description"){
+            if(keyArray[key] == "name"){
                 input.setAttribute("type","text");
+                input.setAttribute("style","width: 22%;"); //TODO: Dummy Values for percent
+                input.value = obj[keyArray[key]];
+                topRow.appendChild(input);
+            }else if(keyArray[key] == "description"){
+                input.setAttribute("type","text");
+                input.setAttribute("style","width: 100%;"); //TODO: Dummy Values for percent
+                input.value = obj[keyArray[key]];
+                bottomRow.appendChild(input);
             }else{
-                input.setAttribute("type","number");
+                input.setAttribute("type", "number");
+                input.setAttribute("style","width: 13%;"); //TODO: Dummy Values for percent
+                input.value = obj[keyArray[key]];
+                topRow.appendChild(input);
             }
-            input.value = obj[keyArray[key]];
-            col.appendChild(input);
-            row.appendChild(col);
         }
-        $("#plant-table").append(row);
+        contentCol.appendChild(topRow);
+        contentCol.appendChild(bottomRow);
+
+        //TODO: Buttons Cols
+        var saveButton = document.createElement("input");
+        saveButton.setAttribute("type","button");
+        saveButton.setAttribute("style","width: 50%; height: 100%");
+        saveButton.value = "S";
+        saveButton.id = "save-button-" + i;
+        var deleteButton = document.createElement("input");
+        deleteButton.setAttribute("type","button");
+        deleteButton.setAttribute("style","width: 50%; height: 100%");
+        deleteButton.value = "D";
+        deleteButton.id = "delete-button-" + i;
+        buttonsCol.appendChild(saveButton);
+        buttonsCol.appendChild(deleteButton);
+
+        fullRow.appendChild(pictureCol);
+        fullRow.appendChild(contentCol);
+        fullRow.appendChild(buttonsCol);
+
+        $("#plant-table").append(fullRow);
     }
 }
