b02404f5874893224b5993169777bdf12ba541e5
HarrySchreiber <44842528+HarrySchreiber@users.noreply.github.com>
2021-02-04 12:36:48 -0500

    Bunch of updates and rewrites, the db can now add via the webapp

--- Admin Portal/frontend/app.js
+++ Admin Portal/frontend/app.js
@@ -1,39 +1,50 @@
 // Lambda API URL
 var API_URL = 'https://695jarfi2h.execute-api.us-east-1.amazonaws.com/production/web';
 
-// Working GET exammple
-var options = { 
-    method: 'GET',
-}
-fetch(API_URL) 
-.then(res => res.json())
-.then(data => {
-    // There was not an error
-    buildTable(data);
-})
-.catch((error) => {
-    // There was an error
-    console.log(error);
-});
 
-// Working POST exammple with empty body
-var options = { 
-    method: 'POST',
-    headers: { 'Content-Type':  'application/json' }, 
-    body: JSON.stringify({})
+// // Working GET exammple
+// var options = { 
+//     method: 'GET',
+//     headers: { 'Content-Type':  'application/json' }
+// }
+// fetch(API_URL,options) 
+// .then(res => res.json())
+// .then(data => {
+//     // There was not an error
+//     console.log(data);
+// })
+// .catch((error) => {
+//     // There was an error
+//     console.log(error);
+// });
+
+function loadPage(){
+    var options = { 
+        method: 'POST',
+        headers: { 'Content-Type':  'application/json' }, 
+        body: JSON.stringify({
+            'operation':'getAll',
+            'tableName':'HydroPotPlantTypes'
+        })
+    }
+    fetch(API_URL,options) 
+    .then(res => res.json())
+    .then(data => {
+        // There was not an error
+        buildTable(data['Items']);
+        console.log(data);
+    })
+    .catch((error) => {
+        // There was an error
+        console.log(error);
+    });
 }
-fetch(API_URL) 
-.then(res => res.json())
-.then(data => {
-    // There was not an error
-    console.log(data);
-})
-.catch((error) => {
-    // There was an error
-    console.log(error);
-});
 
 function buildTable(data){
+    $("#plant-table").empty();
+
+    buildInputFields();
+
     var keyArray = ["name","idealTempHigh","idealTempLow","idealMoistureHigh","idealMoistureLow","idealLightHigh","idealLightLow","description"];
     for(var i = 0; i < data.length; i++){
         //Declare the json object
@@ -41,6 +52,7 @@ function buildTable(data){
         //Declare Columns and rows
         var fullRow = document.createElement("div");
         fullRow.setAttribute("class","row no-gutters");
+        fullRow.setAttribute("id",obj["id"]);
         var pictureCol = document.createElement("div");
         pictureCol.setAttribute("class", "col-md-1 no-gutters");
         var contentCol = document.createElement("div");
@@ -78,16 +90,16 @@ function buildTable(data){
         contentCol.appendChild(topRow);
         contentCol.appendChild(bottomRow);
 
-        //TODO: Buttons Cols
+        //Buttons Cols
         var saveButton = document.createElement("input");
         saveButton.setAttribute("type","button");
         saveButton.setAttribute("style","width: 50%; height: 100%");
-        saveButton.value = "S";
+        saveButton.value = "💾";
         saveButton.id = "save-button-" + i;
         var deleteButton = document.createElement("input");
         deleteButton.setAttribute("type","button");
         deleteButton.setAttribute("style","width: 50%; height: 100%");
-        deleteButton.value = "D";
+        deleteButton.value = "🤮";
         deleteButton.id = "delete-button-" + i;
         buttonsCol.appendChild(saveButton);
         buttonsCol.appendChild(deleteButton);
@@ -98,4 +110,138 @@ function buildTable(data){
 
         $("#plant-table").append(fullRow);
     }
+}
+
+function buildInputFields(){
+
+    var fullRow = document.createElement("div");
+    fullRow.setAttribute("class","row no-gutters");
+    var pictureCol = document.createElement("div");
+    pictureCol.setAttribute("class", "col-md-1 no-gutters");
+    
+
+    var contentCol = document.createElement("div");
+    contentCol.setAttribute("class", "col-md-10 no-gutters");
+
+    var topRow = document.createElement("div");
+    topRow.setAttribute("class","row no-gutters");
+
+    var addPlantName = document.createElement("input");
+    addPlantName.setAttribute("id","add-plant-name");
+    addPlantName.setAttribute("type","text");
+    addPlantName.setAttribute("style","width: 22%");
+    addPlantName.setAttribute("placeholder","Plant Name");
+    topRow.appendChild(addPlantName);
+    var addTempHigh = document.createElement("input");
+    addTempHigh.setAttribute("id","add-temp-high");
+    addTempHigh.setAttribute("type","number");
+    addTempHigh.setAttribute("style","width: 13%");
+    addTempHigh.setAttribute("placeholder","High Temp");
+    topRow.appendChild(addTempHigh);
+    var addTempLow = document.createElement("input");
+    addTempLow.setAttribute("id","add-temp-low");
+    addTempLow.setAttribute("type","number");
+    addTempLow.setAttribute("style","width: 13%");
+    addTempLow.setAttribute("placeholder","Low Temp");
+    topRow.appendChild(addTempLow);
+    var addMoistureHigh = document.createElement("input");
+    addMoistureHigh.setAttribute("id","add-moisture-high");
+    addMoistureHigh.setAttribute("type","number");
+    addMoistureHigh.setAttribute("style","width: 13%");
+    addMoistureHigh.setAttribute("placeholder","High Moisture");
+    topRow.appendChild(addMoistureHigh);
+    var addMoistureLow = document.createElement("input");
+    addMoistureLow.setAttribute("id","add-moisture-low");
+    addMoistureLow.setAttribute("type","number");
+    addMoistureLow.setAttribute("style","width: 13%");
+    addMoistureLow.setAttribute("placeholder","Low Moisture");
+    topRow.appendChild(addMoistureLow);
+    var addLightHigh = document.createElement("input");
+    addLightHigh.setAttribute("id","add-light-high");
+    addLightHigh.setAttribute("type","number");
+    addLightHigh.setAttribute("style","width: 13%");
+    addLightHigh.setAttribute("placeholder","High Light");
+    topRow.appendChild(addLightHigh);
+    var addLightLow = document.createElement("input");
+    addLightLow.setAttribute("id","add-light-low");
+    addLightLow.setAttribute("type","number");
+    addLightLow.setAttribute("style","width: 13%");
+    addLightLow.setAttribute("placeholder","Low Light");
+    topRow.appendChild(addLightLow);
+
+    var bottomRow = document.createElement("div");
+    bottomRow.setAttribute("class","row no-gutters");
+
+    var addDescription = document.createElement("input");
+    addDescription.setAttribute("id","add-description");
+    addDescription.setAttribute("type","text");
+    addDescription.setAttribute("style","width: 100%");
+    addDescription.setAttribute("placeholder","Description");
+    bottomRow.appendChild(addDescription);
+
+    contentCol.appendChild(topRow);
+    contentCol.appendChild(bottomRow);
+
+
+    var buttonsCol = document.createElement("div");
+    buttonsCol.setAttribute("class", "col-md-1 no-gutters");
+    
+    var addButton = document.createElement("input");
+    addButton.setAttribute("id","add-button");
+    addButton.setAttribute("onclick","addPlant()");
+    addButton.value = "🥵";
+    addButton.setAttribute("type","button");
+    addButton.setAttribute("style","width: 100%; height: 100%;");
+    buttonsCol.appendChild(addButton);
+
+    fullRow.appendChild(pictureCol);
+    fullRow.appendChild(contentCol);
+    fullRow.appendChild(buttonsCol);
+
+    $("#plant-table").append(fullRow);
+}
+
+function addPlant(){
+    var plantName = document.getElementById("add-plant-name");
+    var idealMoistureHigh = document.getElementById("add-moisture-high");
+    var idealMoistureLow = document.getElementById("add-moisture-low");
+    var idealLightHigh = document.getElementById("add-light-high");
+    var idealLightLow = document.getElementById("add-light-low");
+    var idealTempHigh = document.getElementById("add-temp-high");
+    var idealTempLow = document.getElementById("add-temp-low");
+    var description = document.getElementById("add-description");
+    var options = { 
+        method: 'POST',
+        headers: { 'Content-Type':  'application/json' }, 
+        body: JSON.stringify({
+            'operation':'add',
+            'tableName':'HydroPotPlantTypes',
+            'payload':{
+                'Item':{
+                    'name':plantName.value,
+                    'idealTempLow':Number(idealTempHigh.value),
+                    'idealTempHigh':Number(idealTempLow.value),
+                    'idealMoistureLow':Number(idealMoistureLow.value),
+                    'idealMoistureHigh':Number(idealMoistureHigh.value),
+                    'idealLightLow':Number(idealLightHigh.value),
+                    'idealLightHigh':Number(idealLightLow.value),
+                    'description':description.value
+                }
+            }
+        })
+        
+    }
+    fetch(API_URL,options) 
+    .then(res => res.json())
+    .then(data => {
+        // There was not an error
+        console.log(data);
+    })
+    .catch((error) => {
+        // There was an error
+        console.log(error);
+    });
+
+
+    setTimeout(() => {loadPage()},2000); //TODO: Fix this asynchronous, the table is being built before the db is updated
 }
