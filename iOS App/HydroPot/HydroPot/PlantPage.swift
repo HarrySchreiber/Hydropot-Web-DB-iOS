e2cf24a64341a5eebfa7a467dcd947cfdec6505f
draydr <draydr17@gcc.edu>
2021-02-10 15:45:37 -0500

    partially linked autowatering to pot.autowatering

--- iOS App/HydroPot/HydroPot/PlantPage.swift
+++ iOS App/HydroPot/HydroPot/PlantPage.swift
@@ -266,6 +266,7 @@ struct PlantPage: View {
             moistureGood = ((pot.curMoisture >= pot.idealMoistureLow) && (pot.curMoisture <= pot.idealMoistureHigh))
             lightGood = (pot.curLight >= pot.idealLightLow && pot.curLight <= pot.idealLightHigh)
             tempGood = (pot.curTemp >= pot.idealTempLow && pot.curTemp <= pot.idealTempHigh)
+            autoWatering = pot.automaticWatering
         }
     }
     func getLastWatered(pot: Pot) -> String {
