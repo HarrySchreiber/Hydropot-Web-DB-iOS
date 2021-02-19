2e434f23d8242ade19d13821e68f96ed9be03757
Spencer Lisle <lislees17@gcc.edu>
2021-02-19 09:59:23 -0500

    updates

--- iOS App/HydroPot/HydroPot/Login.swift
+++ iOS App/HydroPot/HydroPot/Login.swift
@@ -15,40 +15,52 @@ struct Login: View {
     @StateObject var user = GetUser()
     @State var plants = Plants()
     @State var alert = false
+    @State var alertTwo = false
+    @State var confirm: String = ""
+    
+    init() {
+        UISegmentedControl.appearance().setTitleTextAttributes(
+            [
+                .font: UIFont.systemFont(ofSize: UIScreen.main.bounds.size.height >  UIScreen.main.bounds.size.width ?  UIScreen.main.bounds.size.width * 0.05:  UIScreen.main.bounds.size.height * 0.05),
+            ], for: .normal)
+    }
     
     var body: some View {
-        GeometryReader{ geometry in
             ZStack (){
                 VStack (){
                     if (!user.loggedIn) {
                         VStack {
                             Picker(selection: $selectedTab, label: Text("Picker")) {
-                                Text("Login").tag(1)
-                                Text("Sign up").tag(2)
+                                Text("Login")
+                                    .tag(1)
+                                Text("Sign up")
+                                    .tag(2)
                             }
                             .pickerStyle(SegmentedPickerStyle())
-                            .padding(EdgeInsets(top: 15, leading: 25, bottom: 15, trailing: 25))
+                            .padding(EdgeInsets(top: 15, leading: 25, bottom: 5, trailing: 25))
                             if (selectedTab == 1) {
                                 HStack {
                                     TextField("Email", text: $email)
-                                        .padding(6)
-                                        .border(Color.black.opacity(0.2))
+                                        .padding(3)
                                         .frame(minWidth: 0, maxWidth: .infinity)
+                                        .textFieldStyle(PlainTextFieldStyle())
+                                        .border(Color.black.opacity(0.2))
                                 }
-                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 15, trailing: 25))
+                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 5, trailing: 25))
                                 HStack {
                                     SecureField("Password", text: $password)
-                                        .padding(6)
+                                        .padding(3)
                                         .border(Color.black.opacity(0.2))
-                                        .frame(minWidth: 0, maxWidth: .infinity)
+                                        .frame(minWidth: 0, maxWidth:  UIScreen.main.bounds.size.width, minHeight: 0, maxHeight: UIScreen.main.bounds.size.height)
+                                        .textFieldStyle(PlainTextFieldStyle())
                                 }
-                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 15, trailing: 25))
+                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 5, trailing: 25))
                                 Button(action: {
                                     attemptLogin(email: email, password: password)
                                 }) {
                                    Text("Login")
                                     .foregroundColor(.white)
-                                    .padding(10)
+                                    .padding(5)
                                     .background(Color(red: 0.142, green: 0.231, blue: 0.498))
                                     .cornerRadius(6)
                                     .frame(minWidth: 0, maxWidth: .infinity)
@@ -56,32 +68,47 @@ struct Login: View {
                                 .alert(isPresented: $alert) {
                                     Alert(title: Text(""), message: Text("Invalid Login Credentials"), dismissButton: .default(Text("Try Again")))
                                 }
-                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 15, trailing: 25))
+                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 5, trailing: 25))
                             } else {
                                 HStack {
                                     TextField("Name", text: $name)
-                                        .padding(6)
+                                        .padding(3)
                                         .border(Color.black.opacity(0.1))
                                         .frame(minWidth: 0, maxWidth: .infinity)
                                 }
-                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 15, trailing: 25))
+                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 5, trailing: 25))
                                 HStack {
                                     TextField("Email", text: $email)
-                                        .padding(6)
+                                        .padding(3)
                                         .border(Color.black.opacity(0.1))
                                         .frame(minWidth: 0, maxWidth: .infinity)
                                 }
-                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 15, trailing: 25))
+                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 5, trailing: 25))
                                 HStack {
                                     SecureField("Password", text: $password)
-                                        .padding(6)
+                                        .padding(3)
                                         .border(Color.black.opacity(0.1))
                                         .frame(minWidth: 0, maxWidth: .infinity)
                                 }
-                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 15, trailing: 25))
+                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 5, trailing: 25))
+                                HStack {
+                                    SecureField("Re-enter Password", text: $confirm)
+                                        .padding(3)
+                                        .border(Color.black.opacity(0.1))
+                                        .frame(minWidth: 0, maxWidth: .infinity)
+                                }
+                                .alert(isPresented: $alertTwo) {
+                                    Alert(title: Text(""), message: Text("Passwords do not match"), dismissButton: .default(Text("Got it!")))
+                                }
+                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 5, trailing: 25))
                                 Button(action: {
                                     if (name == "" || email == "" || password == ""){
                                         alert = true
+                                    }
+                                    else if (confirm != password){
+                                        if (!(alert == true)){
+                                            alertTwo = true
+                                        }
                                     }
                                     else {
                                         user.signup(name: name, email: email, password: password)
@@ -89,7 +116,7 @@ struct Login: View {
                                 }) {
                                    Text("Sign up")
                                     .foregroundColor(.white)
-                                    .padding(10)
+                                    .padding(5)
                                     .background(Color(red: 0.142, green: 0.231, blue: 0.498))
                                     .cornerRadius(6)
                                     .frame(maxWidth: .infinity)
@@ -100,18 +127,18 @@ struct Login: View {
                                 .padding(EdgeInsets(top: 15, leading: 25, bottom: 15, trailing: 25))
                             }
                         }
-                        .frame(width: geometry.size.width * 0.85, height: geometry.size.height * 0.45, alignment: .center)
-                        .background(Color(UIColor(red: 41.0/255.0, green: 110.0/255.0, blue: 25.0/255.0, alpha: 1.0)))
+                        .frame(width: UIScreen.main.bounds.size.width * 0.8, height: UIScreen.main.bounds.size.height * 0.40, alignment: .center)
+                        .background(Color(red: 41.0/255.0, green: 110.0/255.0, blue: 25.0/255.0, opacity: 0.5))
                         .cornerRadius(6)
                     } else {
                         Home(user: user, plants: plants)
                     }
                 }
-            }
-            .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
-            .onAppear(){
-                plants.getPlantsList()
-            }
+                .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height, alignment: .center)
+                .onAppear(){
+                    plants.getPlantsList()
+                }
+            .font(.system(size:  UIScreen.main.bounds.size.height >  UIScreen.main.bounds.size.width ?  UIScreen.main.bounds.size.width * 0.05:  UIScreen.main.bounds.size.height * 0.05))
             .background(Color.white)
         }
     }
