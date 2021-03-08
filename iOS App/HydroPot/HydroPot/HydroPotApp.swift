116b1265bfe2fc3bea81a8300922fa9201bdf847
Spencer Lisle <lislees17@gcc.edu>
2021-03-08 12:41:34 -0500

    device token progress

--- iOS App/HydroPot/HydroPot/HydroPotApp.swift
+++ iOS App/HydroPot/HydroPot/HydroPotApp.swift
@@ -6,8 +6,8 @@
 //
 
 import SwiftUI
-import UserNotifications
-
+import UIKit
+import Combine
 
 @main
 struct HydroPotApp: App {
@@ -20,44 +20,44 @@ struct HydroPotApp: App {
     }
 }
 
-class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+class AppDelegate: UIResponder, ObservableObject, UIApplicationDelegate {
 
-    func registerForRemoteNotification() {
-        if #available(iOS 10.0, *) {
-            let center  = UNUserNotificationCenter.current()
+    static let shared = AppDelegate()
+    private override init() { super.init() }
 
-            center.requestAuthorization(options: [.sound, .alert, .badge]) { (granted, error) in
-                if error == nil{
-                    UIApplication.shared.registerForRemoteNotifications()
-                }
-            }
+    @Published var deviceToken: String = ""
+    @Published var pushNotification: String = ""
 
-        }
-        else {
-            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.sound, .alert, .badge], categories: nil))
-            UIApplication.shared.registerForRemoteNotifications()
-        }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        UIApplication.shared.registerForRemoteNotifications()
+
+        return true
     }
 
-    
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
-        print(token)
-        
-        UserDefaults.standard.set(token, forKey: "deviceToken")
-        UserDefaults.standard.synchronize()
-
-        print(deviceToken.description)
-        if let uuid = UIDevice.current.identifierForVendor?.uuidString {
-            print(uuid)
+        let tokenParts = deviceToken.map { data -> String in
+            return String(format: "%02.2hhx", data)
         }
-        UserDefaults.standard.setValue(token, forKey: "ApplicationIdentifier")
-        UserDefaults.standard.synchronize()
+        let newToken = tokenParts.joined()
 
-
+        self.deviceToken = newToken
     }
-    
-   
 
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Did Fail to Register for Notification")
+        self.deviceToken = ""
+    }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+
+        do {
+            let data = try JSONSerialization.data(withJSONObject: userInfo, options: .prettyPrinted)
+            pushNotification = String(data: data, encoding: .utf8) ?? "Empty Data"
+        } catch {
+            print("Failed to parse notification with error: \(error.localizedDescription)")
+        }
+
+        completionHandler(.noData)
+    }
 }
 
