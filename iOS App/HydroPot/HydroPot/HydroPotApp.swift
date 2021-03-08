473cbe7c04fb7d1a6714cd8ba4d89d19b0fd1168
Spencer Lisle <lislees17@gcc.edu>
2021-03-07 19:43:45 -0500

    udpates on pinpoint

--- iOS App/HydroPot/HydroPot/HydroPotApp.swift
+++ iOS App/HydroPot/HydroPot/HydroPotApp.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 import UserNotifications
-
+import AWSPinpoint
 
 @main
 struct HydroPotApp: App {
@@ -22,42 +22,78 @@ struct HydroPotApp: App {
 
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
-    func registerForRemoteNotification() {
-        if #available(iOS 10.0, *) {
-            let center  = UNUserNotificationCenter.current()
-
-            center.requestAuthorization(options: [.sound, .alert, .badge]) { (granted, error) in
-                if error == nil{
-                    UIApplication.shared.registerForRemoteNotifications()
-                }
-            }
-
-        }
-        else {
-            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.sound, .alert, .badge], categories: nil))
-            UIApplication.shared.registerForRemoteNotifications()
-        }
-    }
-
+    var pinpoint: AWSPinpoint?
     
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
-        print(token)
+    func application( _: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+             // Instantiate Pinpoint
+             let pinpointConfiguration = AWSPinpointConfiguration
+                 .defaultPinpointConfiguration(launchOptions: launchOptions)
+             // Set debug mode to use APNS sandbox, make sure to toggle for your production app
+             pinpointConfiguration.debug = true
+             pinpoint = AWSPinpoint(configuration: pinpointConfiguration)
+
+             // Present the user with a request to authorize push notifications
+             registerForPushNotifications()
         
-        UserDefaults.standard.set(token, forKey: "deviceToken")
-        UserDefaults.standard.synchronize()
+            AWSDDLog.sharedInstance.logLevel = .verbose
+            AWSDDLog.add(AWSDDTTYLogger.sharedInstance)
 
-        print(deviceToken.description)
-        if let uuid = UIDevice.current.identifierForVendor?.uuidString {
-            print(uuid)
+             return true
+         }
+
+     // MARK: Push Notification methods
+
+     func registerForPushNotifications() {
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, _ in
+            print("Permission granted: \(granted)")
+            guard granted else { return }
+
+            // Only get the notification settings if user has granted permissions
+            self?.getNotificationSettings()
         }
-        UserDefaults.standard.setValue(token, forKey: "ApplicationIdentifier")
-        UserDefaults.standard.synchronize()
+     }
 
+     func getNotificationSettings() {
+         UNUserNotificationCenter.current().getNotificationSettings { settings in
+             print("Notification settings: \(settings)")
+             guard settings.authorizationStatus == .authorized else { return }
 
+             DispatchQueue.main.async {
+                 // Register with Apple Push Notification service
+                 UIApplication.shared.registerForRemoteNotifications()
+             }
+         }
     }
     
-   
+    // MARK: Remote Notifications Lifecycle
+    func application(_: UIApplication,
+                    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
 
+        // Register the device token with Pinpoint as the endpoint for this user
+        pinpoint?.notificationManager
+            .interceptDidRegisterForRemoteNotifications(withDeviceToken: deviceToken)
+    }
+
+    func application(_: UIApplication,
+                    didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: (UNNotificationPresentationOptions) -> Void) {
+         // Handle foreground push notifications
+         pinpoint?.notificationManager.interceptDidReceiveRemoteNotification(notification.request.content.userInfo)
+
+         completionHandler(.badge)
+      }
+
+     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: () -> Void)  {
+         // Handle background and closed push notifications
+         pinpoint?.notificationManager.interceptDidReceiveRemoteNotification(response.notification.request.content.userInfo)
+
+         completionHandler()
+     }
 }
-
