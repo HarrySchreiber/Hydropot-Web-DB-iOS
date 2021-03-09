4449e4cbd26faa2109047abf90c0dc68d42d07a7
Spencer Lisle <lislees17@gcc.edu>
2021-03-09 11:39:26 -0500

    device token added and endpoint made

--- iOS App/HydroPot/HydroPot/HydroPotApp.swift
+++ iOS App/HydroPot/HydroPot/HydroPotApp.swift
@@ -6,8 +6,8 @@
 //
 
 import SwiftUI
-import UIKit
-import Combine
+import UserNotifications
+import AWSPinpoint
 
 @main
 struct HydroPotApp: App {
@@ -22,60 +22,105 @@ struct HydroPotApp: App {
 
 class AppDelegate: UIResponder, ObservableObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
-    static let shared = AppDelegate()
-    private override init() { super.init() }
+    var pinpoint: AWSPinpoint?
 
+    internal func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        // Instantiate Pinpoint
+        let pinpointConfiguration = AWSPinpointConfiguration
+            .defaultPinpointConfiguration(launchOptions: launchOptions)
+        // Set debug mode to use APNS sandbox, make sure to toggle for your production app
+        pinpointConfiguration.debug = true
+        pinpoint = AWSPinpoint(configuration: pinpointConfiguration)
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        UIApplication.shared.registerForRemoteNotifications()
-        UNUserNotificationCenter.current().delegate = self
+        // Present the user with a request to authorize push notifications
         registerForPushNotifications()
+
         return true
     }
 
+    // MARK: Push Notification methods
+
     func registerForPushNotifications() {
-      //1
-      UNUserNotificationCenter.current()
-        //2
+        UNUserNotificationCenter.current().delegate = self
         UNUserNotificationCenter.current()
-          .requestAuthorization(
-            options: [.alert, .sound, .badge]) { [weak self] granted, _ in
-            print("Permission granted: \(granted)")
-            guard granted else { return }
-            self?.getNotificationSettings()
-          }
+            .requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, _ in
+                print("Permission granted: \(granted)")
+                guard granted else { return }
+
+                // Only get the notification settings if user has granted permissions
+                self?.getNotificationSettings()
+            }
     }
 
     func getNotificationSettings() {
-      UNUserNotificationCenter.current().getNotificationSettings { settings in
-        print("Notification settings: \(settings)")
-        guard settings.authorizationStatus == .authorized else { return }
-        DispatchQueue.main.async {
-          UIApplication.shared.registerForRemoteNotifications()
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            print("Notification settings: \(settings)")
+            guard settings.authorizationStatus == .authorized else { return }
+
+            DispatchQueue.main.async {
+                // Register with Apple Push Notification service
+                UIApplication.shared.registerForRemoteNotifications()
+            }
         }
-      }
-    }
-
-    func application(
-      _ application: UIApplication,
-      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-    ) {
-      let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
-      let token = tokenParts.joined()
-      print("Device Token: \(token)")
-    }
-
-    func application(
-      _ application: UIApplication,
-      didFailToRegisterForRemoteNotificationsWithError error: Error
-    ) {
-      print("Failed to register: \(error)")
     }
     
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification,
-        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.list, .banner, .badge, .sound])
+    // MARK: Remote Notifications Lifecycle
+    func application(_: UIApplication,
+                    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+
+        // Register the device token with Pinpoint as the endpoint for this user
+        pinpoint?.notificationManager
+            .interceptDidRegisterForRemoteNotifications(withDeviceToken: deviceToken)
     }
+
+    func application(_: UIApplication,
+                    didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
+    
+    func application(_ application: UIApplication,
+                    didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                    fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult)
+                        -> Void) {
+        // if the app is in the foreground, create an alert modal with the contents
+        if application.applicationState == .active {
+            let alert = UIAlertController(title: "Notification Received",
+                                          message: userInfo.description,
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+
+            UIApplication.shared.keyWindow?.rootViewController?.present(
+                alert, animated: true, completion: nil
+            )
+        }
+
+        // Pass this remote notification event to pinpoint SDK to keep track of notifications produced by AWS Pinpoint campaigns.
+        pinpoint?.notificationManager.interceptDidReceiveRemoteNotification(
+            userInfo)
+
+        completionHandler(.newData)
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: (UNNotificationPresentationOptions) -> Void) {
+         // Handle foreground push notifications
+         pinpoint?.notificationManager.interceptDidReceiveRemoteNotification(notification.request.content.userInfo)
+
+         completionHandler(.badge)
+      }
+
+     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: () -> Void)  {
+         // Handle background and closed push notifications
+         pinpoint?.notificationManager.interceptDidReceiveRemoteNotification(response.notification.request.content.userInfo)
+
+         completionHandler()
+     }
+
 
 }
 
