b33243222fd5e8e071dd6935d4d8100c934aea01
draydr <draydr17@gcc.edu>
2021-04-17 13:18:17 -0400

    added David's iphone to bundle

---
+++ iOS App/HydroPot/HydroPot/TextLimiter.swift
@@ -0,0 +1,28 @@
+//
+//  TextLimiter.swift
+//  HydroPot
+//
+//  Created by David Dray on 4/11/21.
+//
+
+import Foundation
+
+class TextLimiter: ObservableObject {
+    private let limit: Int
+    
+    init(limit: Int) {
+        self.limit = limit
+    }
+    
+    @Published var value = "" {
+        didSet {
+            if value.count > self.limit {
+                value = String(value.prefix(self.limit))
+                self.hasReachedLimit = true
+            } else {
+                self.hasReachedLimit = false
+            }
+        }
+    }
+    @Published var hasReachedLimit = false
+}
